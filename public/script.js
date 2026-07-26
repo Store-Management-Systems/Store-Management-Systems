e47e1527fc598903dc168081dfe2c2778b9ed87f
Kamikaze <pyranoze@gmail.com>
2026-07-26 15:26:53 +0530

    v2 changes

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = '/api';
+const API_URL = 'https://store-management-systems.onrender.com/api';
 let state = { user: null, shopId: null, shops: [], items: [], cart: {} };
 
 // --- Auth & RBAC ---
